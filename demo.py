ee6fa999589f349869d63c19ee261d248dd87a2e
YagneshGondalia <yagnesh.gondalia@gmail.com>
2024-01-12 20:53:00 -0500

    Factorial using recursion

--- demo.py
+++ demo.py
@@ -1,5 +1,22 @@
-def greet():
-    print("Hello")
-    greet()
 
-greet()
+def fact(n):
+    if n == 0:
+        return 1
+    return n * fact(n-1)
+
+
+result = fact(5)
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
