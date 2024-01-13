a4252dec132f32560d8bb97c1278a762db493621
YagneshGondalia <yagnesh.gondalia@gmail.com>
2024-01-12 20:33:21 -0500

    Factorial

--- demo.py
+++ demo.py
@@ -1,18 +1,11 @@
 
-def fib(n):
+def fact(n):
+    f=1
+    for i in range(1,n+1):
+        f=f*i
 
-    a=0
-    b=1
-    print(a)
-    print(b)
+    return f
 
-    for i in range(2,n):
-        c=a+b
-        a=b
-        b=c
-        print(c)
-
-
-fib(10)
-
-
+x =4
+result = fact(x)
+print(result)
